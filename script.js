09469bdb2215876e36a4599dd4b7c455c3611914
Leonardo Pereira <leonardo.pereira1650@outlook.com>
2025-06-09 11:18:55 -0300

    Implement functions

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     envelopeCard.classList.toggle("open");
   });
 });
+
+const botoes = document.querySelectorAll(".botoes");
+const message = document.querySelector(".message");
+const avancarBtn = document.getElementById("avancar");
+const voltarBtn = document.getElementById("voltar");
+
+//esse bloco de codigo cria um array das imagens
+const imagens = message.querySelectorAll("img");
+const arrayImg = [];
+imagens.forEach((imagem) => {
+  arrayImg.push(imagem);
+});
