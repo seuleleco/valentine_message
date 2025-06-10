72dce5fa65ebc72c6b4f3e350ce91434b432fb8b
Leonardo Pereira <leonardo.pereira1650@outlook.com>
2025-06-10 16:43:16 -0300

    corrections

--- script.js
+++ script.js
@@ -7,36 +7,48 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const botoes = document.querySelectorAll(".botoes");
-  const message = document.querySelector(".message");
+  const messageDiv = document.querySelector(".message");
   const avancarBtn = document.getElementById("avancar");
   const voltarBtn = document.getElementById("voltar");
 
+  
+
   //esse bloco de codigo cria um array das imagens
-  const imagens = message.querySelectorAll("img");
+  const imagens = messageDiv.querySelectorAll("img");
   const arrayImg = [];
   imagens.forEach((imagem) => {
     arrayImg.push(imagem);
   });
- console.log(arrayImg);
+
   let i = 0;
 
+  arrayImg.forEach((item, index) => {
+    if (index === 0) {
+      item.style.display = "block"; 
+    } else {
+      item.style.display = "none"; 
+    }
+  }
+) 
    
   function avancar() {
     if (i < arrayImg.length - 1) {
-      i++;
       arrayImg[i].style.display = "none";
-      arrayImg[i - 1].style.display = "block";
+      i++;
+      arrayImg[i].style.display = "block";
+      
     }
   }
   function voltar() {
     if (i > 0) {
-      i--;
       arrayImg[i].style.display = "none";
-      arrayImg[i + 1].style.display = "block";
+      i--;
+      arrayImg[i].style.display = "block";
     }
   }
+  
 
   avancarBtn.addEventListener("click", avancar);
   voltarBtn.addEventListener("click", voltar);
-  console.log(arrayImg);
+  
 });
